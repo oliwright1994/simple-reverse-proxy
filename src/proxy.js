9cc8a2a8f249b95ceff20ffa0cc6389ab0a0088f
oliwright1994 <49248785+oliwright1994@users.noreply.github.com>
2020-09-22 16:48:01 +0100

    fix typo

--- src/proxy.js
+++ src/proxy.js
@@ -6,8 +6,8 @@ let http = require('http'),
 let proxy = httpProxy.createProxyServer();
 
 let server = http.createServer(function (req, res) {
-  subdomain = req.headers.host.split('.')[0]
-  isRailsApp = subdomain.match(/(api|id)/i)
+  let subdomain = req.headers.host.split('.')[0]
+  let isRailsApp = subdomain.match(/(api|id)/i)
 
   let targetHost = isRailsApp ? `${subdomain}.local.autoenrolment.co.uk:3000` : 'localhost:4200'
   proxy.web(req, res, { target: 'http://' + targetHost });
